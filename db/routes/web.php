fbfb28320a20856b2ca22e52954e77cfa0450720
yokkesan <lc.yteshima@gmail.com>
2026-05-13 15:09:05 +0900

    APIテスト用

--- db/routes/web.php
+++ db/routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/hello', function () {
+    return response()->json([
+        'message' => 'hello'
+    ]);
+});
